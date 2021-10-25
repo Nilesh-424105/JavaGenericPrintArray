f5f1702c379e57abd5c1df14ba880e97597d158d
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-25 19:38:55 +0530

    ADDED: Welcome Message

---
+++ com/generic/PrintArray.java
@@ -0,0 +1,13 @@
+package com.generic;
+
+/*
+ * Welcome To Java Generic Print Array Program
+ */
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Java Generic Print Array Program");
+	}
+}
